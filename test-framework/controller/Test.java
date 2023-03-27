847cb273151f27fa54a64618d0c799371f980de2
rakharrs <rodyox.yoann@gmail.com>
2023-03-27 23:47:08 +0300

    Réorganisation

---
+++ test-framework/controller/Test.java
@@ -0,0 +1,10 @@
+package controller;
+
+import etu1999.framework.utils.mapping.Url;
+
+public class Test {
+    @Url("mirindra")
+    public void test(){
+        System.out.println("oko");
+    }
+}
---
+++ test-framework/controller/Test2.java
@@ -0,0 +1,10 @@
+package controller;
+
+import etu1999.framework.utils.mapping.Url;
+
+public class Test2 {
+    @Url("teest2")
+    public void test(){
+        System.out.println("oko");
+    }
+}
